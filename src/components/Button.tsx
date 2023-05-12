7ba873d9d63a00cb431460e1a574c1b029925212
Michael <kilcorse.michael92@gmail.com>
2023-05-11 22:23:05 -0400

    Destructure Button Props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,16 @@
 interface ButtonProps {
-    clickFunc: (event: React.MouseEvent<HTMLButtonElement>) => void,
-    text: string
+  clickFunc: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  text: string;
 }
 
-export default function Button(props: ButtonProps){
-    return(
-        <button type="button" onClick={props.clickFunc} className="border-c64light-purple m-5 border-2">
-            <p className="m-5 font-press-start text-c64light-purple">
-                {props.text}
-            </p>
-        </button>
-    );
+export default function Button({ clickFunc, text }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={clickFunc}
+      className="border-c64light-purple m-5 border-2"
+    >
+      <p className="m-5 font-press-start text-c64light-purple">{text}</p>
+    </button>
+  );
 }
